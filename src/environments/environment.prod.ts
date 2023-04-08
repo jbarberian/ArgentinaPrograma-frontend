f772e752bc64875634c8cc4bd235721500cd3d4e
ignaciobarberia <ignacio.barberia@condortech.com.ar>
2023-04-08 14:48:32 -0300

    Nueva URL para el backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "https://fierce-hollows-40695.herokuapp.com"
+  //apiURL: "https://fierce-hollows-40695.herokuapp.com"
+  apiURL: "https://argentinaprograma2022.onrender.com"
 };
